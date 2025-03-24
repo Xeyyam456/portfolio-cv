4e50fed4d740c6a4224c02d21bc8b0f1bd2efd25
Xeyyam Elizade <xeyyamelizade5@gmail.com>
2025-03-24 17:41:36 +0400

    style.css dosyasi tek dosya

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,34 +1,58 @@
 // src/components/Contact.js
 import React from 'react';
+import Swal from 'sweetalert2'
 
 const Contact = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Add your form submission logic here
-    };
+   
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "e824103a-756a-41da-a374-713d6f562519");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            Swal.fire({
+                title: "Xos gorduk",
+                text: "Mesajiniz ugurla gonderildi!",
+                icon: "success"});
+        }
+      };
+    
 
     return (
         <section id="contact" className="contact">
-            <h2>Contact Me</h2>
-            <form className="contact-form" onSubmit={handleSubmit}>
+            <h2>Menimle elaqe</h2>
+            <form className="contact-form" onSubmit={onSubmit}>
                 <div className="form-row">
                     <div className="form-group">
-                        <input type="text" placeholder="First Name" required />
+                        <input type="text" name='name' placeholder="First Name" required />
                     </div>
                     <div className="form-group">
-                        <input type="text" placeholder="Last Name" required />
+                        <input type="text" name='last_name' placeholder="Last Name" required />
                     </div>
                 </div>
                 <div className="form-row">
                     <div className="form-group">
-                        <input type="email" placeholder="Email" required />
+                        <input type="email" name='email' placeholder="Email" required />
                     </div>
                     <div className="form-group">
-                        <input type="tel" pattern="[0-9]*" placeholder="Phone Number" required />
+                        <input type="tel" pattern="[0-9]*" name='phone' placeholder="Phone Number" required />
                     </div>
                 </div>
                 <div className="form-group">
-                    <textarea placeholder="Your Message" rows="6" required></textarea>
+                    <textarea placeholder="Your Message" name='message' rows="6" required></textarea>
                 </div>
                 <button type="submit" className="submit-btn">Send Message</button>
             </form>
